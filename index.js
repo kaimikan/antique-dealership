d16a39196c64322a0752c46a441e51faf9ac0b10
kaimikan <oihic@protonmail.com>
2024-07-18 19:24:06 +0300

    Single Antique Page DB Requests & Styling

--- index.js
+++ index.js
@@ -31,13 +31,31 @@ async function getAntiques() {
     let antiques = response.rows;
 
     for (let i = 0; i < antiques.length; i++) {
-      console.log(antiques[i]);
+      // console.log(antiques[i]);
       antiques[i].imageObject = await getAntiqueMainImage(antiques[i]);
     }
 
-    console.log({ antiques });
+    // console.log({ antiques });
 
     return antiques;
+  } catch (error) {
+    console.error(error.message);
+    return { error: error.message };
+  }
+}
+
+async function getAntique(id) {
+  try {
+    const response = await db.query('SELECT * FROM antiques WHERE id = $1', [
+      id,
+    ]);
+    let antique = response.rows[0];
+
+    antique.imageObject = await getAntiqueMainImage(antique);
+    antique.secondaryImageObjects = await getAntiqueSecondaryImages(antique);
+    console.log('ANTIQUE: ', { antique });
+
+    return antique;
   } catch (error) {
     console.error(error.message);
     return { error: error.message };
@@ -52,10 +70,35 @@ async function getAntiqueMainImage(antique) {
       antique.main_image_id,
     ]);
     const image = response.rows;
-    console.log({ image });
+    // console.log({ image });
 
     // row returns an array of selection, but we only want one image object from it
     return image[0];
+  } catch (error) {
+    console.error(error.message);
+    return { error: error.message };
+  }
+}
+
+async function getAntiqueSecondaryImages(antique) {
+  try {
+    // getting the main image first
+    // TODO get secondary images
+    const secondaryImagesIds = antique.secondary_images_ids;
+    // console.log({ secondaryImagesIds });
+
+    const imagesIdsString = `(${secondaryImagesIds.join(', ')})`;
+    // console.log(imagesIdsString);
+
+    const response = await db.query(
+      ' SELECT * FROM images WHERE id = ANY($1::int[])',
+      [secondaryImagesIds]
+    );
+    const images = response.rows;
+    console.log(images);
+
+    // row returns an array of selection, but we only want one image object from it
+    return images;
   } catch (error) {
     console.error(error.message);
     return { error: error.message };
@@ -68,6 +111,16 @@ app.get('/', async (req, res) => {
     currentPage: PAGES.home,
     pages: PAGES,
     antiques: antiques,
+  });
+});
+
+app.get('/antique:id', async (req, res) => {
+  console.log(req.params.id);
+  const antique = await getAntique(req.params.id);
+  res.render('antique.ejs', {
+    currentPage: PAGES.home,
+    pages: PAGES,
+    antique: antique,
   });
 });
 
