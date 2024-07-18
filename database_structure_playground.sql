9e3ac7cc3a7e9bfded10d59692ca691ab25a674f
kaimikan <oihic@protonmail.com>
2024-07-18 16:02:52 +0300

    Initial Pagination & Database Connection

---
+++ database_structure_playground.sql
@@ -0,0 +1,60 @@
+-- Antique Dealership
+
+-- Create Read Update Delete
+
+-- reading - everyone with access to the site
+-- creating updating and deleting - only accessible after logging in through a secret page as an admin
+
+-- Structure
+-- admins (id, username, password) - create them only locally without an actual function? so they only have login
+-- images (id, name, path, alt, size)
+-- categories (id, name, description, filter_words)
+-- antiques (id, name, description, category_ids, dimensions_centimeters, cost_euro, reference_number, main_image_id, secondary_images_ids)
+
+CREATE TABLE admins (
+	id SERIAL PRIMARY KEY, 
+	username TEXT NOT NULL,
+	password_hash TEXT NOT NULL, 
+);
+
+INSERT INTO admins (username, password_hash) VALUES ('adminOne', '$2a$12$DlQ6FEbBnfVXz0.WBsYjCuThhekUm5IkVivakQn8rH79QcIjG5p66');
+-- https://bcrypt-generator.com/
+-- adminOne -> 12 rounds bcrypt
+
+CREATE TABLE categories (
+	id SERIAL PRIMARY KEY, 
+	name TEXT NOT NULL,
+	description TEXT NOT NULL, 
+	filter_words TEXT[] NOT NULL
+);
+INSERT INTO categories (name, description, filler_words) 
+VALUES ('chairs', 'a category for all chair antiques', '{"chair","chairs","armchair","armchair"}');
+
+CREATE TABLE images (
+	id SERIAL PRIMARY KEY, 
+	name TEXT NOT NULL,
+	path TEXT NOT NULL, 
+	alt TEXT NOT NULL,
+  img_extension TEXT NOT NULL
+);
+INSERT INTO images (name, path, alt, img_extension) 
+VALUES ('test_img_1', 'assets/images', 'a test image', 'png');
+INSERT INTO images (name, path, alt, img_extension) 
+VALUES ('test_img_2', 'assets/images', 'a second test image', 'jpg');
+
+CREATE TABLE antiques (
+	id SERIAL PRIMARY KEY, 
+	name TEXT NOT NULL,
+	description TEXT NOT NULL, 
+	category_ids INT[] NOT NULL, 
+	dimensions_centimeters TEXT NOT NULL, 
+	cost_euro FLOAT NOT NULL CHECK (cost_euro BETWEEN 0 AND 10000000), 
+	reference_number TEXT NOT NULL, 
+	main_image_id INT NOT NULL REFERENCES images(id), 
+	secondary_images_ids INT[]
+);
+
+INSERT INTO antiques (name, description, category_ids, dimensions_centimeters, cost_euro, reference_number, main_image_id, secondary_images_ids) 
+VALUES ('Example Chair', 'A chair used as an example first antique item', '{1}', 
+'Height: 140cm; Width: 100cm; Length: 90cm;', 50, '#123123', 1, '{2, 2, 2}');
+
--- index.js
+++ index.js
@@ -1,14 +1,90 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import pg from 'pg';
 
 const app = express();
 const port = 3000;
 
+// TODO eventually get this from .env
+const db = new pg.Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'antique_dealership',
+  password: 'grespass',
+  port: 5432,
+});
+db.connect();
+
+const PAGES = {
+  home: 'home',
+  categories: 'categories',
+  contact: 'contact',
+  delivery: 'delivery',
+};
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+async function getAntiques() {
+  try {
+    const response = await db.query('SELECT * FROM antiques');
+    let antiques = response.rows;
+
+    for (let i = 0; i < antiques.length; i++) {
+      console.log(antiques[i]);
+      antiques[i].imageObject = await getAntiqueMainImage(antiques[i]);
+    }
+
+    console.log({ antiques });
+
+    return antiques;
+  } catch (error) {
+    console.error(error.message);
+    return { error: error.message };
+  }
+}
+
+async function getAntiqueMainImage(antique) {
+  try {
+    // getting the main image first
+    // TODO get secondary images
+    const response = await db.query('SELECT * FROM images WHERE id = $1', [
+      antique.main_image_id,
+    ]);
+    const image = response.rows;
+    console.log({ image });
+
+    // row returns an array of selection, but we only want one image object from it
+    return image[0];
+  } catch (error) {
+    console.error(error.message);
+    return { error: error.message };
+  }
+}
+
 app.get('/', async (req, res) => {
-  res.render('index.ejs', {});
+  const antiques = await getAntiques();
+  res.render('index.ejs', {
+    currentPage: PAGES.home,
+    pages: PAGES,
+    antiques: antiques,
+  });
+});
+
+app.get('/contact', async (req, res) => {
+  res.render('contact.ejs', { currentPage: PAGES.contact, pages: PAGES });
+});
+
+app.get('/categories', async (req, res) => {
+  res.render('categories.ejs', { currentPage: PAGES.categories, pages: PAGES });
+});
+
+app.get('/delivery', async (req, res) => {
+  res.render('delivery.ejs', { currentPage: PAGES.delivery, pages: PAGES });
+});
+
+app.get('/login', async (req, res) => {
+  res.render('login.ejs', {});
 });
 
 app.listen(port, () => {
